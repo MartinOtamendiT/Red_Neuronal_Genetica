6fc516ee682a39c7ce9fb62c06ce3e26cf797aa3
Martin Otamendi <89677633+MartinOtamendiT@users.noreply.github.com>
2022-11-25 00:00:14 -0600

    Add Selection and Reproduction

--- genetics.py
+++ genetics.py
@@ -40,7 +40,6 @@ y = df[target_column].values
 #Determinamos K para el algoritmo Stratified KFolds Cross Validation.
 K=2
 skf = StratifiedKFold(n_splits=K, shuffle=True, random_state=1)
-acc_list=[]
 
 #Clase individuo.
 #Permite la creación de un nuevo individuo.
@@ -60,13 +59,13 @@ class Indivual():
                 aux_decimal=self.get_num_neurons2Int()
             
             #Se genera número de capas ocultas.
-            self.hidden_layers = np.random.randint(2, size=3)
+            self.hidden_layers = np.random.randint(2, size=4)
             #Conversión a decimal.
             aux_decimal= self.get_hidden_layers2Int()
             #Se verifica que el número de capas ocultas sea mayor a 0.
             #   Si no es así, se vuelve a generar un valor al azar.
-            while aux_decimal==0:
-                self.hidden_layers = np.random.randint(2, size=3)
+            while aux_decimal==0 or aux_decimal>4:
+                self.hidden_layers = np.random.randint(2, size=4)
                 aux_decimal= self.get_hidden_layers2Int()
             
             #Se genera número de épocas.
@@ -106,16 +105,15 @@ class Indivual():
             self.learning_rate = Chromosome[3]
             self.momentum = Chromosome[4]
         
-        #Se calcula la aptitud (Acc) del individuo llamando a la red neuronal.
-        self.fit=self.call_network()
-        print(f'**Individuo**\nNum_neurons: {self.get_num_neurons2Int()}\nHidden_layers: {self.get_hidden_layers2Int()}\nNum_epochs: {self.get_num_epochs2Int()}\nLearning_rate: {self.get_learning_rate2Float()}\nMomentum: {self.get_momentum_2Float()}\nFitness: {self.fit}')
+        #Se calcula el fitness del individuo.
+        self.calculate_fitness()
     
     #Método que retorna el valor del número de neuronas como entero.
     def get_num_neurons2Int(self):
         return int("".join(str(x) for x in self.num_neurons), 2)
     #Método que retorna el valor del número de neuronas como array de bits.
     def get_num_neurons(self):
-        return self.get_num_neurons
+        return self.num_neurons
 
     #Método que retorna el valor del número de capas ocultas como entero.
     def get_hidden_layers2Int(self):
@@ -144,11 +142,18 @@ class Indivual():
     #Método que retorna el valor del Momentum como array de bits.
     def get_momentum (self):
         return self.momentum
+    def printChromosome(self):
+        print(f'**Individuo**\nNum_neurons: {self.get_num_neurons2Int()}\nHidden_layers: {self.get_hidden_layers2Int()}\nNum_epochs: {self.get_num_epochs2Int()}\nLearning_rate: {self.get_learning_rate2Float()}\nMomentum: {self.get_momentum_2Float()}\nFitness: {self.fit}')
+
+        
     
-    #Entrena a la red neuronal con los genes del individuo.
-    def call_network(self):
+    #Función fitness
+    #Entrena a la red neuronal con los genes del individuo y regresa el
+    # Acc como fitness del individuo.
+    def calculate_fitness(self):
         #Obtenemos una lista para pasarle el parámetro de capas ocultas a la red neuronal.
         capasOcultas = []
+        acc_list=[]
         for i in range (self.get_hidden_layers2Int()):
             capasOcultas.append(self.get_num_neurons2Int())
         #Aplicamos los hiperparámetros necesarios para generar el modelo de la red neuronal (instancia).
@@ -164,31 +169,113 @@ class Indivual():
             #Prueba red neuronal y guarda el resultado en acc_list.
             acc_list.append(mlp.score(x_test_fold, np.ravel(y_test_fold)))
 
-        print('List of possible accuracy:', acc_list)
+        #print('List of possible accuracy:', acc_list)
         #Considera el acc máximo
         accuracy=np.max(acc_list)
-        return accuracy
+        self.fit=accuracy
 
-#Función que crea una poblacion inicial. Se retorna una arreglo de individuos.
-def init_population(population_size):
-    population=[]
-    for i in range(population_size):
-        population.append(Indivual())
-    return population
+class Population():
+    #Constructor que crea una poblacion inicial. Se retorna una arreglo de individuos.
+    def __init__(self, population_size):
+        self.population=[]
+        for i in range(population_size):
+            self.population.append(Indivual())
 
-def genetico():
+    #Selecciona a los mejores individuos de la población.
+    def selection(self, n_selection):
+        #Ordena a los individuos de la población de menor a mayor con base en sus fitness.
+        sorted_population= sorted(self.population, key=lambda x: x.fit)
+        #Selecciona los individuos con mejor fitness y los guarda en una lista
+        selected= sorted_population[len(sorted_population)-n_selection :]
+        #Ordena de mayor a menor a los individuos seleccionados.
+        self.selected= sorted(selected, key=lambda x: x.fit, reverse=True)
+    
+    def reproduction(self):
+        point=0
+        parents=[]
+        
+        for individuo in self.selected:
+            #Selecciona dos padres al azar.
+            parents=random.sample(self.selected, 2)
+
+            #Define punto de separación.
+            point = np.random.randint(1, 3)
+            #Combina las estructuras de ambos padres para el gen: número de neuronas.
+            individuo.get_num_neurons()[:point]=parents[0].get_num_neurons()[:point]
+            individuo.get_num_neurons()[point:]=parents[1].get_num_neurons()[point:]
+
+            #Define punto de separación.
+            point = np.random.randint(1, 3)
+            #Combina las estructuras de ambos padres para el gen: número de neuronas.
+            individuo.get_hidden_layers()[:point]=parents[0].get_hidden_layers()[:point]
+            individuo.get_hidden_layers()[point:]=parents[1].get_hidden_layers()[point:]
+
+            #Define punto de separación.
+            point = np.random.randint(1, 9)
+            #Combina las estructuras de ambos padres para el gen: número de neuronas.
+            individuo.get_num_epochs()[:point]=parents[0].get_num_epochs()[:point]
+            individuo.get_num_epochs()[point:]=parents[1].get_num_epochs()[point:]
+
+            #Define punto de separación.
+            point = np.random.randint(1, 6)
+            #Combina las estructuras de ambos padres para el gen: número de neuronas.
+            individuo.get_learning_rate()[:point]=parents[0].get_learning_rate()[:point]
+            individuo.get_learning_rate()[point:]=parents[1].get_learning_rate()[point:]
+
+            #Define punto de separación.
+            point = np.random.randint(1, 6)
+            #Combina las estructuras de ambos padres para el gen: número de neuronas.
+            individuo.get_momentum()[:point]=parents[0].get_momentum()[:point]
+            individuo.get_momentum()[point:]=parents[1].get_momentum()[point:]
+
+            individuo.calculate_fitness()
+            """print(f"Punto:{point}")
+            print(f"Padre {parents[0].get_num_epochs()}")
+            print(f"Madre {parents[1].get_num_epochs()}")
+            print(f"Nuevo gen: {individuo.get_num_epochs()}")"""
+            self.population.append(individuo)
+            #population[i][:point] = father[0][:point]
+            #population[i][point:] = father[1][point:]
+        #self.population=self.selected
+    
+    def get_best(self):
+        return max(individuo.fit for individuo in self.population)
+
+    def get_min(self):
+        return min(individuo.fit for individuo in self.population)
+    #Checar
+    def get_mean(self):
+        return np.mean(individuo.fit for individuo in self.population)
+            
+            
+
+
+#Algoritmo genético.
+def genetics(population_size, n_generations):
     print("Generando poblacion")
+    ParametrosRNA = Population(population_size)
+    for i in range(n_generations):
+        print(f"**********Generacion {i}**************")
+        ParametrosRNA.selection(10)
+        ParametrosRNA.reproduction()
+        #print(ParametrosRNA.population[8].printChromosome())
+        #print(ParametrosRNA.population[9].printChromosome())
+        print(len(ParametrosRNA.population))
+        print(ParametrosRNA.get_best())
+        print(ParametrosRNA.get_min())
 
-#1.Se genera Poblacion (o posibles soluciones)
-#2.Calculo del fitness (o costo) (seria el ACC)
+
+
+
+#1.Se genera Poblacion (o posibles soluciones)-
+#2.Calculo del fitness (o costo) (seria el ACC)-
 #3.Seleccion de padres
 #4.Crossover o cruza de los padres
 #5.Mutacion en los hijos
 #Repetir varias generaciones
 
 if __name__ == '__main__':
-    population_size=5
-    
-    fish=Indivual()
-    #population=init_population(population_size)
+    genetics(population_size=20, n_generations=2)
+    #fish=Indivual()
+    #print(fish.get_num_epochs())
     
